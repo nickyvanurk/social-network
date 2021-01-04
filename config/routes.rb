af96c1ec5556a0edba3a5c110634e25605de3553
Nicky van Urk <nickyvanurk@gmx.com>
2021-01-04 11:44:50 +0100

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   root 'application#index'
+
+  get 'signup', to: 'devise/users#new'
+  get 'login', to: 'devise/sessions#new'
+  post 'login', to: 'devise/sessions#create'
+  delete 'logout', to: 'devise/sessions#destroy'
+
+  devise_for :users, path_names: {
+    sign_in: 'login', sign_out: 'logout',
+    sign_up: 'signup', registration: 'register'
+  }
 end
