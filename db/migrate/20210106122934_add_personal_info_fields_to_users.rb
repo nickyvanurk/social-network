deca75c6bacd4f3fe98273c57fa6015b7cd0d73f
Nicky van Urk <nickyvanurk@gmx.com>
2021-01-06 15:03:44 +0100

    Add user personal info fields to DB

---
+++ db/migrate/20210106122934_add_personal_info_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddPersonalInfoFieldsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :username, :string, null: false
+    add_column :users, :name, :string
+    add_column :users, :age, :integer, null: false
+    add_column :users, :gender, :string, null: false
+    add_column :users, :country, :string, null: false
+  end
+end
